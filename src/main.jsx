e27a40eb800fdef866f8977b47828fd4ad6bcdd8
yousufrahman <ryousuf766@gmail.com>
2026-04-06 22:04:30 +0600

    added the navbar initial from daisy ui

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import './index.css'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from './Components/ROOT/Home.jsx';
+import Navbar from './Components/Shared/Navbar.jsx';
 
 const router =createBrowserRouter([
   {
     path: '/',
     element : <Home></Home>
+  } ,
+  {
+    path: 'navbar',
+    element : <Navbar></Navbar>
   }
 
 
